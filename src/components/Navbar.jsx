a71abaf6942f750daf6172a38c15445b5928832b
Sakib <168235198+Sakib-Hossain23@users.noreply.github.com>
2025-03-27 21:39:49 +0600

    Update Navbar.jsx

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -91,7 +91,7 @@ const Navbar = ({
               Gadget<span style={{ color: "orange" }}>s</span>
             </h1>
 
-            <p>GoGadget</p>
+            <p>GoGadget Store</p>
           </div>
         </Link>
 
